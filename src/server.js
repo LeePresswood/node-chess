b775657970f1a10b7e9a9bb995a8da7d3d0ce3eb
Lee Presswood <leepresswood@gmail.com>
2024-03-31 11:22:01 -0700

    Added initial server and html files.

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import { createServer } from "http";
+import { readFileSync } from "fs";
+
+const server = createServer((req, res) => {
+    const html = readFileSync("index.html");
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(html);
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
